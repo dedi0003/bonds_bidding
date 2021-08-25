5307ce8b3091bcc03a9fe01e674a071002648130
Dea Avega Editya <dedi0003.student.monash.edu>
2021-08-25 19:41:59 +1000

    add some plots

--- backup_script.R
+++ backup_script.R
@@ -1,3 +1,66 @@
+#script for create UST, Yield, cds, foreign
+fac1 <- readxl::read_xlsx("data/forecast_new.xlsx", col_names = T, sheet = 2) %>%
+  clean_names() %>% as_tibble()
+
+#read inflation
+cpi <- read_csv("data/cpi_worldbank.csv", skip = 4) %>% 
+  filter(`Country Name` == "Indonesia") %>% as_tibble()
+
+cpi_t <- t(cpi[-1:-4]) 
+colnames(cpi_t) <- cpi[ ,1]
+
+cpi_t <- cbind(year = rownames(cpi_t), cpi_t)
+rownames(cpi_t) <- NULL
+
+cpi_t <- cpi_t %>% as_tibble() %>% 
+  mutate(year = as.numeric(year), inflation = as.numeric(Indonesia)) %>% 
+  select(-2) %>% drop_na()
+
+#read ownership
+own <- readxl::read_xlsx("data/kepemilikan asing.xlsx", col_names = T) %>% drop_na() %>%  janitor::clean_names() %>%  as_tibble()
+
+own_total <- read_xlsx("data/TOTAL KEPEMILIKAN.xlsx", col_names = F) %>%
+  rename(date = 1, ownership = 2) %>% 
+  as_tibble()
+
+ownership <- left_join(x = own, y = own_total, by = "date") %>% 
+  filter(date >= date("2015-01-02"))
+
+#combine all factors
+yield_join <- left_join(fac1, ownership, by = "date") %>% 
+  fill(c(ownership, foreign_own), .direction = "down") %>% 
+  mutate(foreign_pct = foreign_own/ownership, year = year(date))
+
+yield_factor <- left_join(yield_join, cpi_t, by = "year") %>% select(-year) %>% 
+  na_replace(inflation, replacement = 1.4)
+
+#impute inflation for 2021
+na_replace(yield_factor$inflation, replacement = 1.4)
+
+yield_factor <- yield_factor %>% 
+  mutate(for_chg = foreign_pct - lag(foreign_pct, default = first(foreign_pct)), 
+         up_down = case_when(for_chg < 0 ~ "down",
+                             for_chg > 0 ~ "up", TRUE ~ "no change")) %>% 
+  rename(domestic_10y = domestik_10y)
+
+#add policy rate
+pol_rate <- read_xlsx("data/policy_rate.xlsx", skip = 4, col_names = T) %>% 
+  mutate(date = as.Date(Tanggal), rate = as.double(parse_number(`BI-7Day-RR`))) %>%
+  select(date, rate)
+
+yield_factors <- left_join(yield_factor, pol_rate, by = "date") %>% 
+  fill(rate, .direction = "down")
+
+unique(ownership_bonds$group)
+
+cek <- ownership_bonds %>% 
+  filter()
+  summarise(date, id, )
+
+yield_factors <- left_join(yield_factor)
+
+
+
 #script for reading ownership 2015
 read_cleansheet <- function(filename, sheet = sheet){
   x <- read_xlsx(filename, col_names = T, sheet = sheet) %>% 
@@ -8,11 +71,11 @@ read_cleansheet <- function(filename, sheet = sheet){
     mutate(id = tolower(sub("[*]", "", id))) %>% 
     mutate(id = gsub(".*/ ","",id)) %>% 
     filter(!(id %in% c("government institution", "non-banks", "non-bank/non-banks"))) %>% 
-    mutate(id = gsub(".*/","",id), date = as.Date(gsub("[/]", "", date), format = "%m%d%Y")) %>% 
-    mutate(group = factor(case_when(id == "bank indonesia" ~ "government",
+    mutate(id = trimws(gsub(".*/","",id)), date = as.Date(gsub("[/]", "", date), format = "%m%d%Y")) %>% 
+    mutate(group = factor(case_when(id %in% c("bank indonesia", "bank indonesia ") ~ "government",
                                     id == "banks" ~ "bank",
                                     id == "incl. foreign government(s) & central bank(s) *" ~ "non-bank (part of foreign holders)",
-                                    id %in% c("mutual funds", "insurance company", "foreign holders", "pension funds", "securities company", "individual", "others")~"non-bank", TRUE ~ "government"))
+                                    id %in% c("mutual funds","mutual fund", "pension fund","insurance", "insurance company", "foreign holders", "pension funds", "securities company", "individual", "others")~"non-bank", TRUE ~ "government"))
     )
 }
 
@@ -178,19 +241,19 @@ read_newsheet <- function(filename, sheet = sheet){
     mutate(type = rep(c("sun", "sbsn", "total"), nrow(merge_test)/3)) %>% 
     mutate(id = tolower(trimws(id))) %>%
     mutate(id = case_when(id == "non resident" ~ "foreign holders", 
-                          str_extract(id, "net") == "net" ~ "bank indonesia",
-                          str_extract(id, "net") == "net" ~ "bank indonesia",
-                          
+                          str_extract(id, "net, excluding") == "net, excluding" ~ "bank indonesia",
+                          id == "-gov't securities used in monetary operation with banks" ~ "-gov't securities used in monetary operation with banks",
                           TRUE ~ id))%>% 
     mutate(date = ymd(date), 
            group = factor(case_when(
-             str_extract(id,"net") == "net" ~ "government",
-             str_extract(id, "monetary operation") == "monetary operation" ~ "government (part of BI)",
-             str_extract(id, "gross") == "gross" ~ "government (part of BI)",
+             id == "bank indonesia" ~ "government",
+             stringr::str_extract(id, "gross") == "gross" ~ "government (part of BI)",
              id %in% c("conventional bank", "islamic bank") ~ "bank",
-             
-             str_extract(id, "foreign government") == "foreign government" ~ "non-bank (part of foreign holders)",
-             id %in% c("mutual funds", "insurance company", "foreign holders", "pension funds", "securities company", "individual", "others")~"non-bank", TRUE ~ "government"))
+             stringr::str_extract(id, "foreign government") == "foreign government" ~ "non-bank (part of foreign holders)",
+             id %in% c("mutual funds","mutual fund", "pension fund","insurance", 
+                       "insurance company", "foreign holders", "pension funds", 
+                       "securities company", "individual", "others", "insurance and pension fund") ~ "non-bank", 
+             TRUE ~ "government (part of BI)"))
     )
 }
 
@@ -244,3 +307,42 @@ ownership_bonds <- rbind(ownership_2015, ownership_2016, ownership_2017,
 
 saveRDS(ownership_bonds, "ownership_bonds.rds")
 
+ownership_bonds %>% 
+  filter(date == "2021-08-02", group %in% c("government (part of BI)", "government"))
+
+
+##add auction date
+auction_2021 <- date(c('2021/01/05', '2021/01/19', '2021/02/02', '2021/02/16', '2021/03/02', '2021/03/16', 
+                   '2021/03/30', '2021/04/13', '2021/04/27', '2021/05/25',
+                   '2021/06/08', '2021/06/22', '2021/07/06', '2021/07/21', 
+                   '2021/08/03', '2021/08/18', '2021/08/31', '2021/09/14', '2021/09/28',
+                   '2021/10/12', '2021/10/26', '2021/11/09', '2021/11/23', '2021/12/07')) %>% as_tibble()
+
+auction_2020 <- date(c('2020/01/07', '2020/01/21', '2020/02/04', '2020/02/18', '2020/03/03', '2020/03/17',
+                       '2020/03/31', '2020/04/14', '2020/04/28', '2020/05/12', '2020/06/02', '2020/06/16', '2020/06/30',
+                       '2020/07/14', '2020/07/28', '2020/08/11', '2020/08/25', '2020/09/08', '2020/09/22', '2020/10/06',
+                       '2020/10/20', '2020/11/03', '2020/11/17', '2020/12/01')) %>% as_tibble()
+
+
+
+auction_2019 <- date(c('2019/01/03', '2019/01/15', '2019/01/29', '2019/02/12', '2019/02/26',
+                       '2019/03/12', '2019/03/26', '2019/04/09', '2019/04/23', '2019/05/07', 
+                       '2019/05/21', '2019/06/18', '2019/07/02', '2019/07/16', '2019/07/30', 
+                       '2019/08/13', '2019/08/27', '2019/09/10', '2019/09/24', '2019/10/08', '2019/10/22',
+                       '2019/11/05', '2019/11/19', '2019/12/03')) %>% as_tibble()
+
+auction_2018 <- date(c('2018/01/03', '2018/01/16', '2018/01/30', '2018/02/13', '2018/02/27', '2018/03/13',
+                       '2018/03/27', '2018/04/10', '2018/04/24', '2018/05/08', '2018/05/22', '2018/06/05',
+                       '2018/07/03', '2018/07/17', '2018/07/31', '2018/08/14', '2018/08/28', '2018/09/12', '2018/09/25',
+                       '2018/10/09', '2018/10/23', '2018/11/06', '2018/11/21', '2018/12/04', '2018/12/18')) %>% as_tibble()
+
+auction_2017 <- date(c('2017/01/03', '2017/01/17', '2017/01/31', '2017/02/14', '2017/02/28', '2017/03/14', '2017/03/27', 
+                       '2017/04/11', '2017/04/25', '2017/05/09', '2017/05/23', '2017/06/06', '2017/06/20', 
+                       '2017/07/11', '2017/07/25', '2017/08/08', '2017/08/22', '2017/09/05', '2017/09/19',
+                       '2017/10/03', '2017/10/17', '2017/10/31', '2017/11/14', '2017/12/05')) %>% as_tibble()
+
+auction_2016 <- date(c('2016/01/05', '2016/01/19', '2016/02/02', '2016/02/16', '2016/03/01', '2016/03/15', '2016/03/29',
+                       '2016/04/12', '2016/04/26', '2016/05/10', '2016/05/24', '2016/06/07', '2016/06/21', '2016/07/19', '2016/08/02', '2016/08/16', '2016/08/30',
+                       '2016/09/13', '2016/09/27', '2016/10/11', '2016/10/25', '2016/11/08')) %>% as_tibble()
+
+auction_date <- rbind(auction_2016, auction_2017, auction_2018, auction_2019, auction_2020, auction_2021)
